99c17ae28c8c62ccd672053bf4082dd524589389
Teo <iftime.teo@gmail.com>
2016-09-11 19:55:35 +0300

    half content

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
-	
-
+	$('arrivals .item #title').mouseenter(function(){
+		$(this).css("color","transparent")
+		.siblings('ul').slideDown(100);
+	})
+	$('arrivals .links').mouseleave(function(){
+		$(this).slideUp(100)
+		.siblings('p#title').css("color","#000");
+	})
 })
